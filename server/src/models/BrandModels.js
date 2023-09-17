264e26f9198afea7cbe02b11ebb6eb7e20981858
Joynal Abedin <joynal.9933@gmail.com>
2023-09-17 22:44:00 +0600

    Brand model

---
+++ server/src/models/BrandModels.js
@@ -0,0 +1,12 @@
+// import mongoose from "mongoose";
+const mongoose = require('mongoose');
+
+const DataSchema = mongoose.Schema({
+    BrandName:{type:String, trim:true, required:true, unique:true},
+    BrandImg:{type:String, trim:true, required:true}
+},{timestamps:true,versionKey:false});
+
+
+const BrandModel =  mongoose.model('brands', DataSchema);
+
+module.exports = BrandModel;
